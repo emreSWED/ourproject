65731cd366e6d92a7b965b54fa9a40a768276459
emreSWED <emre.oezoener@stud.fra-uas.de>
2025-12-03 17:22:17 +0100

    updated current project status to main branch

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,43 @@
 
 import de.tudresden.sumo.cmd.*;
-import de.tudresden.sumo.objects.*;
 import de.tudresden.sumo.util.*;
 import it.polito.appeal.traci.*;
 
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import conn.ConnectionManager;
+
+import conn.ConnectionManager;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        SumoTraciConnection conn = new SumoTraciConnection("sumo-gui", "SumoConfig/myconfig.sumocfg");
-        conn.addOption("start","true");
-        conn.runServer();
+
+        ConnectionManager conn = new ConnectionManager("SumoConfig/myconfig.sumocfg");
+        conn.startConnection();
 
         //get map data for UI
-        List<String> trafficLights = (List<String>) conn.do_job_get(Trafficlight.getIDList());
-        List<String> lanes = (List<String>) conn.do_job_get(Lane.getIDList());
+        List<String> trafficLights = (List<String>) conn.traciConnection.do_job_get(Trafficlight.getIDList());
+        List<String> lanes = (List<String>) conn.traciConnection.do_job_get(Lane.getIDList());
+        JunctionLoader junctions = new JunctionLoader(conn.traciConnection);
 
 
+        System.out.println("current number of traffic lights: " + trafficLights.size());
+        System.out.println("ID of Traffic lights:"+ conn.getTrafficLights());
+        System.out.println("ID of Lanes:"+ lanes);
+        System.out.println("ID of Junctions:" + junctions.JunctionPositionList);
 
         for (int step = 0; step < 10000; step++) {
-            conn.do_timestep();
-            List<String> vehicles = (List<String>) conn.do_job_get(Vehicle.getIDList());
+            conn.step();
 
-            System.out.println("step number " + step + " Number of vehicles in simulation: " + vehicles.size());
-            System.out.println("current traffic lights: " + trafficLights.size());
-            System.out.println("ID of Traffic lights:"+ trafficLights);
-            System.out.println("ID of Lanes:"+ lanes);
-            JunctionLoader junctions = new JunctionLoader(conn);
+            System.out.println("step number " + step + ". Number of vehicles in simulation: " + conn.getVehicles().size());
+            System.out.println("List of cars in simulation: " + conn.getVehicles());
 
-            System.out.println(junctions.JunctionPositionList);
-            //trafficLights.forEach(System.out::println);
-            //System.out.println(trafficLights.toString());
             TimeUnit.MILLISECONDS.sleep(1000);
         }
 
-        conn.close();
+        conn.stopConnection();
         System.out.println("Connection closed.");
     }
 }
