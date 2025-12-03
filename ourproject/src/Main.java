b405019c1138267b71017ec2cb8df5ddea0de15f
emreSWED <emre.oezoener@stud.fra-uas.de>
2025-12-03 12:04:55 +0100

    Updated to get ID of traffic lights and lanes, also added a JunctionLoader class

--- ourproject/src/Main.java
+++ ourproject/src/Main.java
@@ -1,10 +1,12 @@
 
 import de.tudresden.sumo.cmd.*;
+import de.tudresden.sumo.objects.*;
 import de.tudresden.sumo.util.*;
 import it.polito.appeal.traci.*;
 
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -14,16 +16,26 @@ public class Main {
         conn.addOption("start","true");
         conn.runServer();
 
+        //get map data for UI
+        List<String> trafficLights = (List<String>) conn.do_job_get(Trafficlight.getIDList());
+        List<String> lanes = (List<String>) conn.do_job_get(Lane.getIDList());
+
+
+
         for (int step = 0; step < 10000; step++) {
             conn.do_timestep();
             List<String> vehicles = (List<String>) conn.do_job_get(Vehicle.getIDList());
-            List<String> trafficLights = (List<String>) conn.do_job_get(Trafficlight.getIDList());
+
             System.out.println("step number " + step + " Number of vehicles in simulation: " + vehicles.size());
             System.out.println("current traffic lights: " + trafficLights.size());
             System.out.println("ID of Traffic lights:"+ trafficLights);
+            System.out.println("ID of Lanes:"+ lanes);
+            JunctionLoader junctions = new JunctionLoader(conn);
+
+            System.out.println(junctions.JunctionPositionList);
             //trafficLights.forEach(System.out::println);
             //System.out.println(trafficLights.toString());
-            TimeUnit.MILLISECONDS.sleep(50);
+            TimeUnit.MILLISECONDS.sleep(1000);
         }
 
         conn.close();
