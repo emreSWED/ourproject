b59c6504934236dd7240a0806908bbb50635eb2d
emreSWED <emre.oezoener@stud.fra-uas.de>
2025-12-07 17:06:27 +0100

    new branch for testing

---
+++ src/LaneWrapper.java
@@ -0,0 +1,11 @@
+//This class represents a lane with its xpos and ypos and with for rendering in the UI
+
+public class LaneWrapper {
+    private double xposBeginning;
+    private double yposBeginning;
+
+    private double xposEnd;
+    private double yposEnd;
+
+
+}
