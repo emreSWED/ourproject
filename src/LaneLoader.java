20763a98ed6fab983888102f233773d44122441c
emreSWED <emre.oezoener@stud.fra-uas.de>
2025-12-10 13:54:01 +0100

    pushed updated branch to main

--- src/LaneLoader.java
+++ src/LaneLoader.java
@@ -1,29 +1,29 @@
 
 import de.tudresden.sumo.cmd.*;
-import de.tudresden.sumo.objects.SumoGeometry;
-import de.tudresden.sumo.objects.SumoLink;
-import de.tudresden.sumo.objects.SumoLinkList;
-import de.tudresden.sumo.objects.SumoTLSController;
-import de.tudresden.sumo.util.*;
-import it.polito.appeal.traci.*;
+import de.tudresden.sumo.objects.*;
 
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
+
 import conn.ConnectionManager;
 
 public class LaneLoader {
     public static int laneCount; //keeps track of number of lanes
     public static List<String> laneIDs; //list of all the lanes in the simulation
     public static List<SumoGeometry> lanePositions; //List of a List of the singular "lines" the lanes is drawn out of.
+    public static List<SumoPosition2D> atomicLanePositions;
+    public static List<LaneToRender> lanesToRender;
 
     //when a LaneLoader object is created, all the lanes are loaded into the Lists.
     public LaneLoader(ConnectionManager conn) throws Exception {
         laneCount = (int) conn.dojobget(Lane.getIDCount()); //gets the number of lanes into laneCount
         laneIDs = (List<String>) conn.dojobget(Lane.getIDList());
-        lanePositions = new ArrayList<>();//creates a list of all the lanes in laneIDs
+
+        lanePositions       = new ArrayList<>();//creates a list of all the lanes in laneIDs
+        atomicLanePositions = new ArrayList<>();
+        lanesToRender       = new ArrayList<>();
+
         for(int i = 0; i < laneCount; i++){ //loops through laneIDs List and grabs those coordinate List of the lines
             lanePositions.add(i, (SumoGeometry) conn.dojobget(Lane.getShape(laneIDs.get(i))));
         }
@@ -31,9 +31,14 @@ public class LaneLoader {
 
     public static void printAllLaneCoordinates(){
         for(int i = 0; i < laneCount; i++) {
-            System.out.println(lanePositions.get(i));
-            System.out.println("\n");
+            for(int j = 0; j < lanePositions.get(i).coords.size(); j++){
+                atomicLanePositions.add(lanePositions.get(i).coords.get(j));
+            }
         }
+        System.out.println(atomicLanePositions);
+    }
+    public static void convertToRenderLaneCoordinates(){
+
     }
     public static void printAllLaneIDs(){
         System.out.println(laneIDs);
