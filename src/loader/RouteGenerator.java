dcb7fd8706fdb184c38b45ba19b1ea26255ef4ed
emreSWED <emre.oezoener@stud.fra-uas.de>
2025-12-14 17:46:41 +0100

    Vehicel addition version 0.1

---
+++ src/loader/RouteGenerator.java
@@ -0,0 +1,9 @@
+package loader;
+
+import de.tudresden.sumo.cmd.Route;
+import util.ConnectionManager;
+
+public class RouteGenerator {
+    public static ConnectionManager conn;
+   // public Route generateRandomRoute() {}
+}
---
+++ src/loader/VehicleAdder.java
@@ -0,0 +1,9 @@
+package loader;
+import model.MyVehicle;
+import util.ConnectionManager;
+
+public class VehicleAdder {
+
+    public static ConnectionManager conn;
+   // public MyVehicle addRandomVehicle() {}
+}
