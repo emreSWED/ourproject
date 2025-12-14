cbb5ba970f5c7a9f31dd8d3c5bb85124630c7281
emreSWED <emre.oezoener@stud.fra-uas.de>
2025-12-14 15:27:11 +0100

    Lanes Render to Path2D objects for Graphics2D library, with flipped y coordinates to accomodate for swings top left (0,0) indexing and SUMOs bottom left (0,0) indexing.

--- src/loader/LaneLoader.java
+++ src/loader/LaneLoader.java
@@ -13,7 +13,8 @@ public class LaneLoader {
     public static int laneCount; //keeps track of number of lanes
     public static List<String> laneIDs; //list of all the lanes in the simulation
     public static List<SumoGeometry> lanePositions; //List of a List of the singular "lines" the lanes is drawn out of.
-    public static List<SumoPosition2D> atomicLanePositions;
+    public static List<MyLane> myLanes;
+   // public static ConnectionManager conn; instead of having conn in the Constructor, it could also be declared static here
 
     //when a loader.LaneLoader object is created, all the lanes are loaded into the Lists.
     public LaneLoader(ConnectionManager conn) throws Exception {
@@ -21,24 +22,21 @@ public class LaneLoader {
         laneIDs = (List<String>) conn.dojobget(Lane.getIDList());
 
         lanePositions       = new ArrayList<>();//creates a list of all the lanes in laneIDs
-        atomicLanePositions = new ArrayList<>();
+        myLanes = new ArrayList<>();
 
         for(int i = 0; i < laneCount; i++){ //loops through laneIDs List and grabs those coordinate List of the lines
             lanePositions.add(i, (SumoGeometry) conn.dojobget(Lane.getShape(laneIDs.get(i))));
         }
-    }
+        //static connection to conn for all lanes
 
-    public static void printAllLaneCoordinates(){
-        for(int i = 0; i < laneCount; i++) {
-            for(int j = 0; j < lanePositions.get(i).coords.size(); j++){
-                atomicLanePositions.add(lanePositions.get(i).coords.get(j));
-            }
+        for(int i = 0; i < laneIDs.size(); i++){
+            myLanes.add(new MyLane(laneIDs.get(i), lanePositions.get(i).coords.size() ));
+            //adds the laneID to our own MyLane class, constructor handles loading into Path2D for each lane to be rendered in Graphics2D engine
         }
-        System.out.println(atomicLanePositions);
-    }
-    public static void convertToRenderLaneCoordinates(){
 
     }
+
+
     public static void printAllLaneIDs(){
         System.out.println(laneIDs);
     }
